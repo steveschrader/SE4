dcfadc9a4a11e0a954ca610301cda6470a30b13c
steveschrader <steveschrader@steves-MacBook-Pro.local>
2015-05-23 23:24:02 +0200

    nog een merge

--- SE4-Team9-Opdracht3-code-150501/pajotterswebapp/src/main/java/be/odisee/pajotter/domain/Administrator.java
+++ SE4-Team9-Opdracht3-code-150501/pajotterswebapp/src/main/java/be/odisee/pajotter/domain/Administrator.java
@@ -6,17 +6,16 @@ import javax.persistence.*;
 @Entity
 @DiscriminatorValue("Administrator")
 public class Administrator extends Rol {
-	//Dit word niet gepersisteerd, dit is een rol en er wordt niet overbij gehouden in de DD.
+	
 	//Dit word niet gepersisteerd, dit is een rol en wordt niet overbij gehouden in de DD.
 	public Administrator(){}
 
     public Administrator(String status, String usernaam, Partij partij){
-        super(status,usernaam,partij);
         super(status, usernaam, partij);
     }
 
     public Administrator(int id, String status, String usernaam, Partij partij){
-        super(id,status,usernaam,partij);
+        super(id, status, usernaam, partij);
     }
 	
 	@Override
